ffe1b4ee872dd4f0c75ab29172838978c9339e08
MayorGit <amayowa@lachestry.com>
2023-02-03 00:35:50 +0300

    Add Webpack
    [#INDEV22-328]

---
+++ webpack.config.js
@@ -0,0 +1,149 @@
+const path = require('path');
+const buildPath = path.resolve(__dirname, 'dist');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    devtool: 'eval-cheap-module-source-map',
+    entry: './src/index.ts',
+    output: {
+        filename: 'index.[hash:20].js',
+        path: buildPath
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                include: [path.resolve(__dirname, 'src')]
+            },
+            {
+                test: /\.json$/,
+                type: 'javascript/auto',
+                loader: 'custom-json-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    {
+                        // creates style nodes from JS strings
+                        loader: "style-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        // translates CSS into CommonJS
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        // compiles Sass to CSS
+                        loader: "sass-loader",
+                        options: {
+                            outputStyle: 'expanded',
+                            sourceMap: true,
+                            sourceMapContents: true
+                        }
+                    }
+                    // Please note we are not running postcss here
+                ]
+            },
+            {
+                // Load all images as base64 encoding if they are smaller than 8192 bytes
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            // On development we want to see where the file is coming from, hence we preserve the [path]
+                            name: '[path][name].[ext]?hash=[hash:20]',
+                            limit: 8192
+                        }
+                    }
+                ]
+            },
+            {
+                // Load all icons
+                test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.(scss|css|sass)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            outputStyle: 'expanded',
+                            sourceMap: true,
+                            sourceMapContents: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },  
+    devServer: {
+        port: 8888,
+        contentBase: buildPath
+    },
+    resolve: {
+        fallback: {
+          fs: false
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        }),
+        new OptimizeCssAssetsPlugin({
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: {
+                map: {
+                    inline: false,
+                },
+                discardComments: {
+                    removeAll: true
+                },
+                discardUnused: false
+            },
+            canPrint: true
+        })
+    ]
+}
